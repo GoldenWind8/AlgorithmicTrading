e8cdec89ea33b37f304f90eb039d9ca5483011dc
CrazyDiamond <sashinchetty8@gmail.com>
2022-04-25 18:54:24 +0200

    Added use of matplotlib finance to plot candlestick chart

--- DataVisualization.py
+++ DataVisualization.py
@@ -1,21 +1,19 @@
 import datetime as dt
 import matplotlib.pyplot as plt
 from matplotlib import style
+import mplfinance as mpf
+import matplotlib.dates as mdates
 import pandas as pd
 import pandas_datareader.data as web
 
 df = pd.read_csv('tsla.csv', parse_dates= True, index_col=0)
 #df[['Open', 'Close', 'High']].plot()
 #plt.show()
-print(df.head(7))
 
 #Creating a moving average column
 df['100ma'] = df['Adj Close'].rolling(window =100).mean()
 
-print(df.head())
-print(df.tail())
 df.dropna(inplace=True)
-print(df.head())
 
 #Graphing info with matplotlib
 ax1 = plt.subplot2grid((6,1), (0,0), 4, 1)
@@ -24,4 +22,31 @@ ax2 = plt.subplot2grid((6,1), (5,0), 2, 1, sharex = ax1)  #ShareX links the axes
 ax1.plot(df.index , df['Adj Close'])
 ax1.plot(df.index, df['100ma'])
 ax2.bar(df.index, df['Volume'])
+#plt.show()
+
+
+#Remoddling Data - We want to resample daily prices to 10 day data
+df_ohlc = df['Adj Close'].resample('10D').ohlc()
+df_volume = df['Volume'].resample('10D').sum()
+
+print("Resampled Data")
+print(df_ohlc)
+print(df_volume)
+
+
+#Using mplfinance package:
+df_ohlc.reset_index(inplace = True)  #Resetting date as not index
+print(df_ohlc)
+
+#Print Candlestick chart
+mpf.plot(df, type='candle', style='charles',
+            title='  ',
+            ylabel='  ',
+            ylabel_lower='  ',
+            figratio=(25,10),
+            figscale=1,
+            mav=50,
+            volume=True
+            )
+
 plt.show()
